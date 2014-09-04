69fd3746a7795efb66ae39d5c742d60a9d7de82d
wytor <wuyuanfu250@126.com>
2014-09-04 18:23:55 +0800

    Initial Commit

---
+++ RemitSilverNotify/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RemitSilverNotify
+//
+//  Created by Oliver on 14-9-4.
+//  Copyright (c) 2014年 iOS_Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
